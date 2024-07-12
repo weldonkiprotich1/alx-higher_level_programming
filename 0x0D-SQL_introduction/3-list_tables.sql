c633efaef02c5d524ab46ddab016a6f184a13bd5
weldonkiprotich1 <weldonkiprotich1@gmail.com>
2024-07-12 02:35:59 -0700

    list tables

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,2 @@
---script that lists all the tables of a database in your MySQL server.
---Query that returns all the tables in a database
+-- Lists all the tables of my database in my SQL
 SHOW TABLES;
